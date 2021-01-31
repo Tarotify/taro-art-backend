a9a1fef8440cfadabca24875866ea0a261b885f8
Microrrr <76843436+Microrrr@users.noreply.github.com>
2021-01-31 13:52:13 -0500

    Update email template

--- email_template/index.js
+++ email_template/index.js
@@ -17,17 +17,35 @@ module.exports = {
     `
     return temp
   },
+  // <div style="padding: 76px 111px; background: #F0F6F9;">
   password_reset: function(param) {
     const temp =
     `
-      <div style="padding: 20px 35px; background: #ebebeb;">
-      <img style="width: 100%" src="https://github.com/Tarotify/taro-art-frontend/blob/master/src/asset/img/taro_email_header.png?raw=true" />
-      Hi:<br/>
-      Your are reseting your account Password. Verify code is ${param}. If you are not doing this prompt, please ignore or contact us, and also reset your account password by yourself.</br>
-      <br/>
-      Thanks,
-      Taro Team
+    <div style="background: #F0F6F9; width:100%; padding: 60px 0;">
+      <div style="background: #FFFFFF; max-width: 600px; margin: auto;">
+        <img style="width: 100%" src="https://github.com/Tarotify/taro-art-frontend/blob/master/src/asset/img/email-header.png?raw=true" />
+        <div style="padding: 24px 60px 117px 60px; color: #471A66;; font-family: Inter; font-style: normal; font-weight: 500;font-size: 15px; line-height: 28px;">
+        Hi,
+        <br/>
+        <br/>
+        Your are reseting your account Password. Verify code is <b>${param}</b>.</br>
+        If you are not doing this prompt, please ignore or contact us, and also reset your account password by yourself.
+        </br>
+        </br>
+        Thanks,<br>
+        Taro Team
+        </div>
+        <p style="font-family: Inter; font-style: normal; font-weight: bold; font-size: 18px;line-height: 22px;text-align: center;color: #471A66;">
+          JOURNEY WITH TARO
+        </p>
+        <p style="margin:7px auto 25px; width:270px; color: rgba(71, 26, 102, 0.8); font-family: Inter;font-style: normal;font-weight: 500;font-size: 11px;line-height: 15px; text-align: center;">
+          Sign up for email newsletters from TARO to receive updates on new events and promotional offers. You can unsubscribe at any time.
+        </p>
+        <p style="padding-bottom: 50px;font-family: Inter;font-style: normal;font-weight: 500;font-size: 11px;line-height: 13px;text-align: center; color: #471A66;">
+          If you have any questions, please contact taro2020@gmail.com
+        </p>
       </div>
+    </div>
     `
     return temp
   }
